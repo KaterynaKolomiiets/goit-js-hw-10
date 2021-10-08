b47873e8e35011f5427377ec24302c820581e808
KaterynaKolomiiets <kateryna.kolomiiets.official@gmail.com>
2021-10-08 11:35:33 +0300

    try

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,8 +2,8 @@ export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`,
   ).then(response => {
-    if (!response) {
-      throw new Error("haha");
+    if (response.status >= 400 && response.status < 500) {
+      throw new Error('Oops, there is no country with that name');
     }
     return response.json();
   });
--- src/index.js
+++ src/index.js
@@ -1,48 +1,47 @@
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries';
-import debounce from 'lodash.debounce'
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
-const countryCardEl = document.querySelector(".country-info")
+const countryCardEl = document.querySelector('.country-info');
 
 const onInput = function () {
-  clearAll()
+  clearAll();
   let name = inputEl.value.trim();
 
-    fetchCountries(name)
-      .then(data => {
-        if (data.length > 10) {
-          Notify.info('Too many matches found. Please enter a more specific name.');
-        }
-        if (data.length >= 2 && data.length <= 10) {
-          renderCountryList(data);
-        }
-        if (data.length === 1) {
-          renderCountryCard(data);
-        }
-      })
-      .catch(error => console.log(error));
-}
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        renderCountryList(data);
+      }
+      if (data.length === 1) {
+        renderCountryCard(data);
+      }
+    })
+    .catch(error => Notify.failure('Oops, something went wrong!'));
+};
 
-
-
-
-    
-const renderCountryList = function (arr) {
-    let list = arr.map(item => `<li><img class="flag-icon" src=${item.flag}>${item.name}</li>`).join("");
-    countryListEl.innerHTML = list
- }
-const renderCountryCard = function ([{ flag, name, capital, population, languages}] = item){
-    let text = `<div class="flex"><img class="big-flag" src=${flag}><h1>${name}</h1></div><b>Capital:</b><span>${capital}</span><b>Population:</b><span>${population}</span><b>Languages:</b><span>${languages.map((item)=>item.name)}</span>`
-    countryCardEl.innerHTML = text
-}
+const renderCountryList = function (countries) {
+  let list = countries
+    .map(item => `<li><img class="flag-icon" src=${item.flag}>${item.name}</li>`)
+    .join('');
+  countryListEl.innerHTML = list;
+};
+const renderCountryCard = function ([{ flag, name, capital, population, languages }] = item) {
+  let text = `<div class="flex"><img class="big-flag" src=${flag}><h1>${name}</h1></div><b>Capital:</b><span>${capital}</span><b>Population:</b><span>${population}</span><b>Languages:</b><span>${languages.map(
+    item => item.name,
+  )}</span>`;
+  countryCardEl.innerHTML = text;
+};
 const clearAll = function () {
-    countryListEl.innerHTML = "";
-    countryCardEl.innerHTML = "";
-}
+  countryListEl.innerHTML = '';
+  countryCardEl.innerHTML = '';
+};
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
